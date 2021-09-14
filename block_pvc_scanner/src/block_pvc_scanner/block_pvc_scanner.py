44d1e9962d54155886423c0e3fa592eafa988ab7
Balazs Hamorszky <balihb@gmail.com>
2021-09-14 02:33:30 +0200

    add other pvc data

--- block_pvc_scanner/src/block_pvc_scanner/block_pvc_scanner.py
+++ block_pvc_scanner/src/block_pvc_scanner/block_pvc_scanner.py
@@ -16,9 +16,27 @@ print_log = logging.StreamHandler()
 print_log.setFormatter(formatter)
 logger.addHandler(print_log)
 
-gauge = Gauge(
+percent_gauge = Gauge(
     'pvc_usage',
     "fetching pvc usage matched by k8s csi",
+    ['volumename']
+)
+
+total_bytes_gauge = Gauge(
+    'pvc_usage_total_bytes',
+    "PVC total bytes",
+    ['volumename']
+)
+
+used_bytes_gauge = Gauge(
+    'pvc_usage_used_bytes',
+    "PVC used bytes",
+    ['volumename']
+)
+
+free_bytes_gauge = Gauge(
+    'pvc_usage_free_bytes',
+    "PVC free bytes",
     ['volumename']
 )
 
@@ -56,13 +74,21 @@ def main():
                 elif gke_data_re.match(possible_pvc):
                     volume = 'pvc' + possible_pvc.split('pvc')[-1]
 
-            pvc_usage = psutil.disk_usage(mount_point).percent
-            logger.info(f'VOLUME: {volume}, USAGE: {pvc_usage}')
-            gauge.labels(volume).set(pvc_usage)
+            disk_usage = psutil.disk_usage(mount_point)
+            logger.info(f'VOLUME: {volume}, USAGE: {disk_usage.percent}')
+
+            percent_gauge.labels(volume).set(disk_usage.percent)
+            total_bytes_gauge.labels(volume).set(disk_usage.total)
+            used_bytes_gauge.labels(volume).set(disk_usage.used)
+            free_bytes_gauge.labels(volume).set(disk_usage.free)
+
             labels.add(volume)
 
         for label in old_labels - labels:
-            gauge.remove(label)
+            percent_gauge.remove(label)
+            total_bytes_gauge.remove(label)
+            used_bytes_gauge.remove(label)
+            free_bytes_gauge.remove(label)
         old_labels = labels
 
         time.sleep(15)
