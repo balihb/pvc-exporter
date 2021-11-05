72f787905d92a98b1f1e0f2f2aeaf58dca91803a
Balazs Hamorszky <balihb@gmail.com>
2021-11-05 14:28:30 +0100

    test for pod pvc mapping

--- pod_pvc_mapping/src/pod_pvc_mapping/main.py
+++ pod_pvc_mapping/src/pod_pvc_mapping/main.py
@@ -1,17 +1,27 @@
 import logging
 import os
 from abc import ABC, abstractmethod
-from collections import namedtuple
 from time import sleep
-from typing import Any
+from typing import NamedTuple, cast
 
 from kubernetes import client, config
-from kubernetes.client import CoreV1Api
+from kubernetes.client import CoreV1Api, V1PersistentVolumeClaimList, V1PersistentVolumeClaim, V1PodList, V1Pod, \
+    V1ObjectMeta, V1PodSpec, V1Volume, V1Namespace, V1NamespaceList, V1PersistentVolumeClaimSpec, \
+    V1PersistentVolumeClaimVolumeSource
 from lazy import lazy
 from prometheus_client import start_http_server, Gauge
 
-Volume = namedtuple('Volume', ['vol', 'ns', 'pod'])
-PodsPvcs = namedtuple('PodsPvcs', ['pods', 'pvcs'])
+
+class Volume(NamedTuple):
+    vol: str
+    ns: str
+    pod: str
+
+
+class PodsPvcs(NamedTuple):
+    pods: list[V1Pod]
+    pvcs: list[V1PersistentVolumeClaim]
+
 
 formatter = logging.Formatter(os.getenv('APP_LOG_FORMAT', '%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger = logging.getLogger(__name__)
@@ -26,47 +36,58 @@ gauge = Gauge(
 )
 
 
-def process_pods(pods: list[Any], pool: dict[Volume, str], pvcs: list[Any], ns: str, new_pool_keys: set[Volume]):
+def process_pods(
+    pods: list[V1Pod],
+    pool: dict[Volume, str],
+    pvcs: list[V1PersistentVolumeClaim],
+    ns: str,
+    new_pool_keys: set[Volume]
+):
     for pod in pods:
-        logger.debug(f"pod: {pod['metadata']['name']}")
+        pod_name = cast(V1ObjectMeta, pod.metadata).name
+        logger.debug(f"pod: {pod_name}")
         try:
-            logger.debug('volumes:')
-            logger.debug(
-                list(
-                    map(
-                        lambda voc: voc['persistent_volume_claim']['claim_name'],
-                        list(filter(lambda vocl: vocl['persistent_volume_claim'], pod['spec']['volumes']))
-                    )
+            pvc_names: list[str] = list(
+                map(
+                    lambda voc: cast(
+                        V1PersistentVolumeClaimVolumeSource,
+                        cast(V1Volume, voc).persistent_volume_claim).claim_name,
+                    list(filter(
+                        lambda vol: cast(V1Volume, vol.persistent_volume_claim),
+                        cast(list[V1Volume], cast(V1PodSpec, pod.spec).volumes)
+                    ))
                 )
             )
-            for vc in pod['spec']['volumes']:
-                if vc['persistent_volume_claim']:
-                    logger.debug(f"claim: {vc['persistent_volume_claim']['claim_name']}")
-                    process_pvc(
-                        pvc=vc['persistent_volume_claim']['claim_name'],
-                        pvcs=pvcs,
-                        pod_name=pod['metadata']['name'],
-                        ns=ns,
-                        pool=pool,
-                        new_pool_keys=new_pool_keys
-                    )
+            logger.debug('volumes:')
+            logger.debug(pvc_names)
+            for pvc_name in pvc_names:
+                logger.debug(f"claim: {pvc_name}")
+                process_pvc(
+                    pvc_name=pvc_name,
+                    pvcs=pvcs,
+                    pod_name=pod_name,
+                    ns=ns,
+                    pool=pool,
+                    new_pool_keys=new_pool_keys
+                )
         except TypeError:
             pass
 
 
-def process_pvc(pvc: str, pvcs: list[Any], pod_name: str, pool: dict[Volume, str], ns: str, new_pool_keys: set[Volume]):
-    for v in pvcs:
-        logger.debug(f"vol name: {v['metadata']['name']}")
-        logger.debug(f'pvc: {pvc}')
-        if v['metadata']['name'] == pvc:
-            vol_name = v['spec']['volume_name']
-            logger.info("NS: %s, POD: %s, VOLUME: %s, PVC: %s" % (ns, pod_name, vol_name, pvc))
+def process_pvc(pvc_name: str, pvcs: list[V1PersistentVolumeClaim], pod_name: str, pool: dict[Volume, str], ns: str,
+                new_pool_keys: set[Volume]):
+    for pvc in pvcs:
+        pvc_metadata_name = cast(V1ObjectMeta, pvc.metadata).name
+        logger.debug(f"vol name: {pvc_metadata_name}")
+        logger.debug(f'pvc: {pvc_name}')
+        if pvc_metadata_name == pvc_name:
+            vol_name = cast(V1PersistentVolumeClaimSpec, pvc.spec).volume_name
+            logger.info("NS: %s, POD: %s, VOLUME: %s, PVC: %s" % (ns, pod_name, vol_name, pvc_name))
             vol = Volume(vol=vol_name, pod=pod_name, ns=ns)
             new_pool_keys.add(vol)
-            if vol in pool.keys():
-                gauge.remove(pvc, vol.vol, vol.pod, vol.ns)
-            gauge.labels(pvc, vol.vol, vol.pod, vol.ns)
-            pool[vol] = pvc
+            if vol not in pool.keys():
+                gauge.labels(pvc_name, vol.vol, vol.pod, vol.ns)
+            pool[vol] = pvc_name
 
 
 def cleanup_pool(pool: dict[Volume, str], old_pool_keys: set[Volume], new_pool_keys: set[Volume]) -> set[Volume]:
@@ -76,59 +97,68 @@ def cleanup_pool(pool: dict[Volume, str], old_pool_keys: set[Volume], new_pool_k
     return set(pool.keys())
 
 
-def get_items(obj):
-    obj_dict = obj.to_dict()
-    items = obj_dict['items']
-    return items
-
-
 class KubeClient(ABC):
 
     @abstractmethod
-    def list_namespace(self) -> list[Any]:
+    def list_namespace(self) -> list[V1Namespace]:
         pass
 
     @abstractmethod
-    def list_namespaced_pod(self, ns: str) -> list[Any]:
+    def list_namespaced_pod(self, ns: str) -> list[V1Pod]:
         pass
 
     @abstractmethod
-    def list_namespaced_persistent_volume_claim(self, ns: str) -> list[Any]:
+    def list_namespaced_persistent_volume_claim(self, ns: str) -> list[V1PersistentVolumeClaim]:
         pass
 
-    @abstractmethod
     def get_single_namespace_data(self, ns: str) -> PodsPvcs:
-        pass
+        logger.debug(f'ns: {ns}')
+        pods: list[V1Pod] = self.list_namespaced_pod(ns)
+        # logger.debug(pods)
+        pvcs: list[V1PersistentVolumeClaim] = self.list_namespaced_persistent_volume_claim(ns)
+        logger.debug('pvcs:')
+        logger.debug(list(
+            map(lambda v: (cast(V1ObjectMeta, v.metadata).name, cast(V1PersistentVolumeClaimSpec, v.spec).volume_name),
+                pvcs)))
+        return PodsPvcs(pods=pods, pvcs=pvcs)
 
 
-class KubeClientImpl(KubeClient):
+class KubeClientImpl(KubeClient):  # pragma: no cover
 
     @lazy
     def k8s_api_client(self) -> CoreV1Api:
         config.load_incluster_config()
         return client.CoreV1Api()
 
-    def list_namespace(self) -> list[Any]:
-        return get_items(self.k8s_api_client.list_namespace())
+    def list_namespace(self) -> list[V1Namespace]:
+        return cast(V1NamespaceList, self.k8s_api_client.list_namespace()).items
 
-    def list_namespaced_pod(self, ns: str) -> list[Any]:
-        return get_items(self.k8s_api_client.list_namespaced_pod(ns))
+    def list_namespaced_pod(self, ns: str) -> list[V1Pod]:
+        return cast(V1PodList, self.k8s_api_client.list_namespaced_pod(ns)).items
 
-    def list_namespaced_persistent_volume_claim(self, ns: str) -> list[Any]:
-        return get_items(self.k8s_api_client.list_namespaced_persistent_volume_claim(ns))
-
-    def get_single_namespace_data(self, ns: str) -> PodsPvcs:
-        logger.debug(f'ns: {ns}')
-        pods: list[Any] = self.list_namespaced_pod(ns)
-        # logger.debug(pods)
-        pvcs: list[Any] = \
-            self.list_namespaced_persistent_volume_claim(ns)
-        logger.debug('pvcs:')
-        logger.debug(list(map(lambda v: (v['metadata']['name'], v['spec']['volume_name']), pvcs)))
-        return PodsPvcs(pods=pods, pvcs=pvcs)
+    def list_namespaced_persistent_volume_claim(self, ns: str) -> list[V1PersistentVolumeClaim]:
+        return cast(V1PersistentVolumeClaimList, self.k8s_api_client.list_namespaced_persistent_volume_claim(ns)).items
 
 
-def main(argv: list[str] = None, kube_client: KubeClient = KubeClientImpl()):
+def main_loop(
+    pool: dict[Volume, str],
+    old_pool_keys: set[Volume],
+    kube_client: KubeClient = KubeClientImpl(),
+) -> set[Volume]:
+    new_pool_keys: set[Volume] = set[Volume]()
+
+    nss: list[V1Namespace] = kube_client.list_namespace()
+    # logger.debug(nss)
+    for i in nss:
+        ns: str = cast(V1ObjectMeta, i.metadata).name
+        pods_pvcs = kube_client.get_single_namespace_data(ns)
+        if len(pods_pvcs.pvcs) != 0 and len(pods_pvcs.pods) != 0:
+            process_pods(pods_pvcs.pods, pool, pods_pvcs.pvcs, ns, new_pool_keys)
+
+    return cleanup_pool(pool=pool, old_pool_keys=old_pool_keys, new_pool_keys=new_pool_keys)
+
+
+def main(argv: list[str] = None, kube_client: KubeClient = KubeClientImpl()):  # pragma: no cover
     pool: dict[Volume, str] = {}
 
     old_pool_keys: set[Volume] = set[Volume]()
@@ -136,17 +166,11 @@ def main(argv: list[str] = None, kube_client: KubeClient = KubeClientImpl()):
     start_http_server(os.getenv('APP_HTTP_SERVER_PORT', 8849))
 
     while 1:
-        new_pool_keys: set[Volume] = set[Volume]()
-
-        nss: list[Any] = kube_client.list_namespace()
-        # logger.debug(nss)
-        for i in nss:
-            ns: str = i['metadata']['name']
-            pods_pvcs = kube_client.get_single_namespace_data(ns)
-            if len(pods_pvcs.pvcs) != 0 and len(pods_pvcs.pods) != 0:
-                process_pods(pods_pvcs.pods, pool, pods_pvcs.pvcs, ns, new_pool_keys)
-
-        old_pool_keys = cleanup_pool(pool=pool, old_pool_keys=old_pool_keys, new_pool_keys=new_pool_keys)
+        old_pool_keys = main_loop(
+            pool=pool,
+            old_pool_keys=old_pool_keys,
+            kube_client=kube_client
+        )
 
         logger.info(f'PVCs found: {len(pool)}')
 
---
+++ pod_pvc_mapping/tests/test_pvc.py
@@ -0,0 +1,102 @@
+import unittest
+from typing import NamedTuple, cast
+
+from kubernetes.client import V1PersistentVolumeClaim, V1Pod, V1Namespace, V1ObjectMeta, V1PodSpec, V1Volume, \
+    V1PersistentVolumeClaimVolumeSource, V1PersistentVolumeClaimSpec, V1Container
+
+from pod_pvc_mapping import main as ppm
+from pod_pvc_mapping.main import KubeClient
+from pod_pvc_mapping.main import Volume
+
+
+class TstEnv(NamedTuple):
+    nss: list[V1Namespace]
+    pods: list[V1Pod]
+    pvcs: list[V1PersistentVolumeClaim]
+
+
+class TstKubeClientImpl(KubeClient):
+    nss: list[V1Namespace]
+    pods: list[V1Pod]
+    pvcs: list[V1PersistentVolumeClaim]
+
+    def __init__(
+        self,
+        test_env: TstEnv
+    ):
+        self.nss = test_env.nss
+        self.pods = test_env.pods
+        self.pvcs = test_env.pvcs
+
+    def list_namespace(self) -> list[V1Namespace]:
+        return self.nss
+
+    def list_namespaced_pod(self, ns: str) -> list[V1Pod]:
+        return list(filter(
+            lambda pod: cast(V1ObjectMeta, pod.metadata).namespace == ns,
+            self.pods
+        ))
+
+    def list_namespaced_persistent_volume_claim(self, ns: str) -> list[V1PersistentVolumeClaim]:
+        return list(filter(
+            lambda pvc: cast(V1ObjectMeta, pvc.metadata).namespace == ns,
+            self.pvcs
+        ))
+
+
+class TestPVC(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.test_env_1 = TstEnv(
+            nss=[
+                V1Namespace(
+                    metadata=V1ObjectMeta(
+                        name='tst_ns_1'
+                    )
+                )
+            ],
+            pods=[
+                V1Pod(
+                    metadata=V1ObjectMeta(
+                        name='tst_pod_1',
+                        namespace='tst_ns_1'
+                    ),
+                    spec=V1PodSpec(
+                        containers=[
+                            V1Container(
+                                name='tst_pod_1_cnt'
+                            )
+                        ],
+                        volumes=[
+                            V1Volume(
+                                name='tst_vol',
+                                persistent_volume_claim=V1PersistentVolumeClaimVolumeSource(
+                                    claim_name='tst_claim_1'
+                                )
+                            )
+                        ]
+                    )
+                )
+            ],
+            pvcs=[
+                V1PersistentVolumeClaim(
+                    metadata=V1ObjectMeta(
+                        name='tst_claim_1',
+                        namespace='tst_ns_1'
+                    ),
+                    spec=V1PersistentVolumeClaimSpec(
+                        volume_name='pvc-2f0c4198-821b-4629-ab8f-59a993e3cd00'
+                    )
+                )
+            ]
+        )
+        self.tst_kube_client_1 = TstKubeClientImpl(
+            test_env=self.test_env_1
+        )
+
+    def test_env_1_start(self):
+        assert ppm.main_loop(
+            old_pool_keys=set[Volume](),
+            pool={},
+            kube_client=self.tst_kube_client_1
+        )
